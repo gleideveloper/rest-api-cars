d78c570a076071213dd8f2a8156bf12560db3183
Gleides Vinente <gleidevelop@gmail.com>
2024-04-09 17:57:50 -0300

    Adicionado LOGS

--- api/src/main/java/com/portal/api/controller/OwnerPostController.java
+++ api/src/main/java/com/portal/api/controller/OwnerPostController.java
@@ -2,6 +2,8 @@ package com.portal.api.controller;
 
 import com.portal.api.dto.OwnerPostDTO;
 import com.portal.api.service.OwnerPostService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,11 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/owner")
 public class OwnerPostController {
 
+    private final Logger LOG = LoggerFactory.getLogger(CarPostController.class);
+
     @Autowired
     private OwnerPostService ownerPostService;
 
     @PostMapping
     public ResponseEntity<Void> createOwnerCar(@RequestBody OwnerPostDTO ownerPostDTO){
+        LOG.info("USANDO REST API - Criando Novo Usuário: {}", ownerPostDTO);
+
         ownerPostService.createOwnerCar(ownerPostDTO);
         return ResponseEntity.ok().build();
     }
